dc2c2456c007a9ebdf3c10fc3e7ae7bb1527b8ae
bencoster <ben@mycyberisland.com>
2023-03-29 21:19:29 +1000

    Update install.py
    These comments explain the purpose of each section of the script and provide better context for understanding the code.

--- install.py
+++ install.py
@@ -1,10 +1,16 @@
 import launch
 import os
 
-req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+# Construct the path to the requirements.txt file in the video2video folder inside the extensions folder
+req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extensions", "video2video", "requirements.txt")
 
+# Open the requirements.txt file
 with open(req_file) as file:
+    # Iterate through each line (library) in the requirements.txt file
     for lib in file:
+        # Remove any leading/trailing whitespace from the library name
         lib = lib.strip()
+        # Check if the library is not already installed
         if not launch.is_installed(lib):
+            # Install the library using pip and display a message indicating the purpose of the installation
             launch.run_pip(f"install {lib}", f"video2video requirement: {lib}")
